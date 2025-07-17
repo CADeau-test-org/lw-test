f220e7a331ea6191e36b2b3608a1191fb07ee15b
laura <laurawang122@gmail.com>
2025-07-16 20:48:06 -0400

    changes

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/thatdog.jpg"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={736}
+          height={530}
           priority
         />
         <ol>
